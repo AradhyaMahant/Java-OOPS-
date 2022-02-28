34d6c01bd4bce39bbb9ccb02fcb8e1b937ac07ea
Aradhya <aradhyamahant@gmail.com>
2022-02-28 08:44:14 +0530

    sum of all int between 40-250 divisible by 5

---
+++ Sum40_250.java
@@ -0,0 +1,18 @@
+package com.R177219040.q4;
+
+public class Sum40_250 {
+	public static void main(String args[])
+		{
+			int sum=0;
+			for(int i=41;i<250;i++)
+				{
+				  if(i%5 == 0)
+				  	{
+					  sum=sum+i;
+				  	}
+				}
+			System.out.println("Sum of all integerts between 40 to 250 div by 5="+sum);
+		}	
+	
+
+}

2b990074065b6515eda5ad2e7e5cd0aa17344017
Aradhya <aradhyamahant@gmail.com>
2022-03-02 14:43:09 +0530

    show that private member of a super class cannot be accessed from derived classes

---
+++ Not_accessed.java
@@ -0,0 +1,13 @@
+package com.R177219040.q1;
+
+public class Not_accessed {
+	void display1()
+		{
+			System.out.println("output from public member");
+		}
+	private void display2()
+	{
+		System.out.println("output from private member");
+	}
+	
+}
---
+++ Not_accessed2.java
@@ -0,0 +1,11 @@
+package com.R177219040.q1;
+
+public class Not_accessed2 extends Not_accessed {
+	public static void main(String args[])
+		{
+			Not_accessed obj = new Not_accessed ();
+			obj.display1();
+			obj.display2();
+		}
+
+}

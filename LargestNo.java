67f96bacf88b1d7503df1dc4692ca3cba7a977c9
Aradhya <aradhyamahant@gmail.com>
2022-04-07 08:29:44 +0530

    finding the largest number

---
+++ LargestNo.java
@@ -0,0 +1,14 @@
+class LargestNo
+{
+public static void main(String args[])
+ {
+   int a=5,b=13,c=88;
+   if(a>b && b>c)
+     System.out.println("a=5 is the largest no.");
+   else if(b>a && a>c)
+     System.out.println("b=13 is the largest no.");
+   else
+     System.out.println("c=88 is the largest no.");
+ }
+}
+    
